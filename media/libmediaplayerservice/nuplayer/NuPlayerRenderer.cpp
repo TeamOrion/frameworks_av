4610f8268aec6eb358bd627df8576c09848ab10d
Dhananjay Kumar <dhakumar@codeaurora.org>
2016-02-25 13:48:33 +0530

    nuplayer: ignore openAudioSink while waiting for teardown completion
    - AudioSink is reopened on AudioTearDown event as well as on
      decoder output format change. These events are from different
      contexts, independent of each other and can occur concurrently,
      doing set of thread-unsafe operations on audioSink, which
      sometimes may result in fatal error.
    
    - Any request to openSink, after teardown event is received but
      before its completed, is redundant since once teardown is
      completed sink is anyway going to be opened again. Also
      ignoring sink reopen while in teardown would avoid concurrent
      processing of thread-unsafe operations on audioSink.
    
    - Update nuplayer to prevent reopening of sink while teardown is
      still going on.
    
    Conflicts:
    	media/libmediaplayerservice/nuplayer/NuPlayer.cpp
    
     CRs-Fixed: 978013
    
    Change-Id: Ieb3825f133c2099e07d6e92ef101dd5156af8a20
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -124,7 +124,7 @@ NuPlayer::Renderer::Renderer(
       mNextAudioClockUpdateTimeUs(-1),
       mLastAudioMediaTimeUs(-1),
       mAudioOffloadPauseTimeoutGeneration(0),
-      mAudioTornDown(false),
+      mAudioTearingDown(false),
       mCurrentOffloadInfo(AUDIO_INFO_INITIALIZER),
       mCurrentPcmInfo(AUDIO_PCMINFO_INITIALIZER),
       mTotalBuffersQueued(0),
@@ -675,6 +675,12 @@ void NuPlayer::Renderer::onMessageReceived(const sp<AMessage> &msg) {
             break;
         }
 
+        case kWhatAudioTearDownComplete:
+        {
+            onAudioTearDownComplete();
+            break;
+        }
+
         case kWhatAudioOffloadPauseTimeout:
         {
             int32_t generation;
@@ -901,7 +907,7 @@ void NuPlayer::Renderer::drainAudioQueueUntilLastEOS() {
 
 bool NuPlayer::Renderer::onDrainAudioQueue() {
     // do not drain audio during teardown as queued buffers may be invalid.
-    if (mAudioTornDown) {
+    if (mAudioTearingDown) {
         return false;
     }
     // TODO: This call to getPosition checks if AudioTrack has been created
@@ -1742,10 +1748,10 @@ bool NuPlayer::Renderer::getSyncQueues() {
 }
 
 void NuPlayer::Renderer::onAudioTearDown(AudioTearDownReason reason) {
-    if (mAudioTornDown) {
+    if (mAudioTearingDown) {
         return;
     }
-    mAudioTornDown = true;
+    mAudioTearingDown = true;
 
     int64_t currentPositionUs;
     sp<AMessage> notify = mNotify->dup();
@@ -1790,6 +1796,11 @@ status_t NuPlayer::Renderer::onOpenAudioSink(
         uint32_t flags) {
     ALOGV("openAudioSink: offloadOnly(%d) offloadingAudio(%d)",
             offloadOnly, offloadingAudio());
+
+    if (mAudioTearingDown) {
+        ALOGW("openAudioSink: not opening now!, would happen after teardown");
+        return OK;
+    }
     bool audioSinkChanged = false;
 
     int32_t numChannels;
@@ -1985,7 +1996,6 @@ status_t NuPlayer::Renderer::onOpenAudioSink(
     if (audioSinkChanged) {
         onAudioSinkChanged();
     }
-    mAudioTornDown = false;
     return OK;
 }
 
@@ -1993,6 +2003,14 @@ void NuPlayer::Renderer::onCloseAudioSink() {
     mAudioSink->close();
     mCurrentOffloadInfo = AUDIO_INFO_INITIALIZER;
     mCurrentPcmInfo = AUDIO_PCMINFO_INITIALIZER;
+}
+
+void NuPlayer::Renderer::signalAudioTearDownComplete() {
+    (new AMessage(kWhatAudioTearDownComplete, this))->post();
+}
+
+void NuPlayer::Renderer::onAudioTearDownComplete() {
+    mAudioTearingDown = false;
 }
 
 }  // namespace android
