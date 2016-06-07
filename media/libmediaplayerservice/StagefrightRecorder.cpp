f462e36983c1ada693493c334da60169a96b2bcb
Lalit Kansara <lkansara@codeaurora.org>
2016-06-07 12:10:35 +0530

    audio: Compilation fix for AOSP flip NRD56
    This is compilation fix for AOSP flip NRD56
    
    Change-Id: I68f0af41c8751bdf1a50c548e98a96e64e26d07f
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -954,9 +954,7 @@ sp<MediaCodecSource> StagefrightRecorder::createAudioSource() {
                 mOpPackageName,
                 sourceSampleRate,
                 mAudioChannels,
-                mSampleRate,
-                mClientUid,
-                mClientPid);
+                mSampleRate);
 
     status_t err = audioSource->initCheck();
 
